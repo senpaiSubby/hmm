0263d27cd9a1847ef8b4547ce7e2a768495a18f1
SimplySublime <koryfreese@gmail.com>
2019-09-15 23:26:14 -0700

    Cleanup

--- __main__.py
+++ __main__.py
@@ -1,45 +1,4 @@
-from quart import (
-    Quart,
-    render_template,
-    session,
-    request,
-    redirect,
-    url_for
-    )
-from os import system
-from app.backend import *
-
-system("clear")
-
-app = Quart(__name__)
-
-@app.route("/", methods=["GET"])
-async def home():
-    return await render_template("/index.html", notes=listNotes())
-
-
-@app.route("/addnote", methods=["POST"])
-async def nadd():
-    form = await request.form
-    print(form)
-    if form["inputbar"]:
-        addNote(form["inputbar"])
-
-    return redirect(url_for("home"))
-
-@app.route("/editnote", methods=["POST"])
-async def nedit():
-    form = await request.form
-    if form["editNote"] and form["inputbar"]:
-        editNote(form["editNote"], form["inputbar"])
-    return redirect(url_for("home"))
-
-@app.route("/delnote", methods=["POST"])
-async def ndel():
-    form = await request.form
-    if form["deleteNote"]:
-        delNote(form["deleteNote"])
-    return redirect(url_for("home"))
+from hmm import app
 
 
 if __name__ == "__main__":
