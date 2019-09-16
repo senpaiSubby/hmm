025397bc508306851c748d2329423db135fa420f
SimplySublime <koryfreese@gmail.com>
2019-09-16 00:08:16 -0700

    Cleanup

--- __main__.py
+++ __main__.py
@@ -2,4 +2,7 @@ from hmm import app
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    try:
+        app.run(debug=True, host="0.0.0.0", port=5000)
+    except KeyboardInterrupt:
+        pass
