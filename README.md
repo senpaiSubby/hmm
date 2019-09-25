b76768cb4c0a2e3046247d4c8a34cce22ed1af34
SimplySublime <koryfreese@gmail.com>
2019-09-24 19:52:35 -0700

    cleanup

--- README.md
+++
@@ -1,3 +0,0 @@
-# hmm
-### Simple note-taking web app powered by MongoDB and Quart
-![Alt Text](https://i.imgur.com/rHF65Wa.gif)
--- static/js/main.js
+++
@@ -1,5 +0,0 @@
-function editnote(buttonid) {
-    var fired_button = $(buttonid).val();
-    alert(fired_button);
-    document.getElementById("inputbar").value = document.getElementById(fired_button).innerText;
-};
